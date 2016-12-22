65b3dd8badfbaca2b0adc8a80e22658c3860afbc
Brian Marx <marxb@c02qnc4pfvh5.local>
2016-12-22 15:58:58 -0500

    Initial Commit Filter frequency does not work

--- SimpleSynth/ViewController.swift
+++ SimpleSynth/ViewController.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
+    
+    var osc: AKOscillator?
+    var filter: AKLowPassFilter?
+    var mixer: AKMixer?
+    var mainFilterFreq = 2000.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        osc = AKOscillator(waveform: AKTable(.sawtooth))
+        filter = AKLowPassFilter(osc!, cutoffFrequency: mainFilterFreq)
+        filter?.start()
+        mixer = AKMixer(filter!)
+        mixer?.start()
+        
+        AudioKit.output = mixer
+        AudioKit.start()
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func OscSlider(_ sender: UISlider) {
+        osc?.frequency = pow(10, Double(sender.value))
+    }
+    
+    @IBAction func FilterSlider(_ sender: UISlider) {
+        filter?.stop()
+        filter?.cutoffFrequency = pow(10, Double(sender.value))
+        mainFilterFreq = (filter?.cutoffFrequency)!
+        filter?.start()
+    }
+    
+    @IBAction func PlayStop(_ sender: UIButton) {
+        
+        if (osc?.isPlaying)! {
+            osc?.stop()
+            sender.setTitle("Play", for: .normal)
+            sender.setTitleColor(UIColor.blue, for: .normal)
+
+        } else {
+            osc?.start()
+            sender.setTitle("Stop", for: .normal)
+            sender.setTitleColor(UIColor.red, for: .normal)
+        }
     }
 
     override func didReceiveMemoryWarning() {
