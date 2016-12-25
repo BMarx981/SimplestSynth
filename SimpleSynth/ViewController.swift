9b52cf931c56499affec0364dcdb472a6ed34d70
Brian Marx <b.marx981@gmail.com>
2016-12-25 12:03:47 -0500

    IT's Alive!!!!!! Working multiple oscillators through  Filter.

--- SimpleSynth/ViewController.swift
+++ SimpleSynth/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     
     var osc1: AKOscillator?
     var osc2: AKOscillator?
+    var osc3: AKOscillator?
+    var osc4: AKOscillator?
     var oscMixer: AKMixer?
     var filter: AKLowPassFilter?
     var mixer: AKMixer?
@@ -24,7 +26,9 @@ class ViewController: UIViewController {
         
         osc1 = AKOscillator(waveform: AKTable(.sawtooth))
         osc2 = AKOscillator(waveform: AKTable(.square))
-        oscMixer = AKMixer(osc1!, osc2!)
+        osc3 = AKOscillator(waveform: AKTable(.triangle))
+        osc4 = AKOscillator(waveform: AKTable(.sine))
+        oscMixer = AKMixer(osc1!, osc2!, osc3!, osc4!)
         filter = AKLowPassFilter(oscMixer!, cutoffFrequency: mainFilterFreq, resonance: mainFilterRes)
         filter?.start()
         mixer = AKMixer(filter!)
@@ -39,6 +43,8 @@ class ViewController: UIViewController {
     @IBAction func OscSlider(_ sender: UISlider) {
         osc1?.frequency = pow(10, Double(sender.value))
         osc2?.frequency = pow(10, Double(sender.value))
+        osc3?.frequency = pow(10, Double(sender.value))
+        osc4?.frequency = pow(10, Double(sender.value))
     }
     
     @IBAction func ResSlider(_ sender: UISlider) {
@@ -50,9 +56,23 @@ class ViewController: UIViewController {
             case 0:
                 osc1?.amplitude = 1.0
                 osc2?.amplitude = 0.0
+                osc3?.amplitude = 0.0
+                osc4?.amplitude = 0.0
             case 1:
                 osc1?.amplitude = 0.0
                 osc2?.amplitude = 1.0
+                osc3?.amplitude = 0.0
+                osc4?.amplitude = 0.0
+            case 2:
+                osc1?.amplitude = 0.0
+                osc2?.amplitude = 0.0
+                osc3?.amplitude = 1.0
+                osc4?.amplitude = 0.0
+            case 3:
+                osc1?.amplitude = 0.0
+                osc2?.amplitude = 0.0
+                osc3?.amplitude = 0.0
+                osc4?.amplitude = 1.0
             default: break
         }//end switch
     }
@@ -62,12 +82,16 @@ class ViewController: UIViewController {
         if (osc1?.isPlaying)! {
             osc1?.stop()
             osc2?.stop()
+            osc3?.stop()
+            osc4?.stop()
             sender.setTitle("Play", for: .normal)
             sender.setTitleColor(UIColor.blue, for: .normal)
 
         } else {
             osc1?.start()
             osc2?.start()
+            osc3?.start()
+            osc4?.start()
             sender.setTitle("Stop", for: .normal)
             sender.setTitleColor(UIColor.red, for: .normal)
         }
